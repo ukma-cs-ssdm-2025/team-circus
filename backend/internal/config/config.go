6ebe32de855060c2d46a657eac453e8e1f38935f
velosypedno <klyuchka.artur@gmail.com>
2025-10-04 21:08:40 +0300

    feat: update config

--- backend/internal/config/config.go
+++ backend/internal/config/config.go
@@ -2,7 +2,16 @@ package config
 
 import "github.com/kelseyhightower/envconfig"
 
+type DBConfig struct {
+	Driver string `envconfig:"DB_DRIVER" required:"true"`
+	Host   string `envconfig:"DB_HOST" required:"true"`
+	Port   string `envconfig:"DB_PORT" required:"true"`
+	User   string `envconfig:"DB_USER" required:"true"`
+	Pass   string `envconfig:"DB_PASSWORD" required:"true"`
+	Name   string `envconfig:"DB_NAME" required:"true"`
+}
 type Config struct {
+	DB DBConfig
 }
 
 func Load() (*Config, error) {
