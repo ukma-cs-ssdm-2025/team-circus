10ae8819820aef0c66b73a825c2b833a96663494
velosypedno <klyuchka.artur@gmail.com>
2025-10-05 12:39:32 +0300

    feat(back): add group endpoints to apiV1

--- backend/internal/app/setup.go
+++ backend/internal/app/setup.go
@@ -2,14 +2,27 @@ package app
 
 import (
 	"github.com/gin-gonic/gin"
+	grouphandler "github.com/ukma-cs-ssdm-2025/team-circus/internal/handler/group"
+	grouprepo "github.com/ukma-cs-ssdm-2025/team-circus/internal/repo/group"
+	groupservice "github.com/ukma-cs-ssdm-2025/team-circus/internal/service/group"
 )
 
 func (a *App) setupRouter() *gin.Engine {
 	router := gin.Default()
 
-	_ = router.Group("/api")
-	{
+	groupRepo := grouprepo.NewGroupRepository(a.db)
+	groupService := groupservice.NewGroupService(groupRepo)
 
+	apiV1 := router.Group("/api/v1")
+	{
+		groups := apiV1.Group("/groups")
+		{
+			groups.POST("", grouphandler.NewCreateGroupHandler(groupService))
+			groups.GET("/:uuid", grouphandler.NewGetGroupHandler(groupService))
+			groups.GET("", grouphandler.NewGetAllGroupsHandler(groupService))
+			groups.PUT("/:uuid", grouphandler.NewUpdateGroupHandler(groupService))
+			groups.DELETE("/:uuid", grouphandler.NewDeleteGroupHandler(groupService))
+		}
 	}
 	return router
 }
