8502a2251bbdb3a22df498e742fad6e52b168c5e
Oleksii Kostyk <kit.melancholic@gmail.com>
2025-10-26 18:20:20 +0200

    fix: imports

--- frontend/src/components/groups/index.ts
+++ frontend/src/components/groups/index.ts
@@ -1,2 +1,3 @@
-export { default as GroupItem } from './GroupItem';
-export { default as GroupsList } from './GroupsList';
+export { default as GroupItem } from "./GroupItem";
+export { default as GroupsList } from "./GroupsList";
+export { default as GroupFormDialog } from "./GroupFormDialog";
